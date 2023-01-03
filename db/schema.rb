2a2b9a515b2b139c6065d79379791b69b9331729
Julia Grimes <julia.s.grimes@gmail.com>
2023-01-03 12:24:33 -0500

    Experimenting with model for game, probably unnecessary and will delete later

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_032206) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_03_042058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_032206) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "found"
   end
 
   create_table "users", force: :cascade do |t|
