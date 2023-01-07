c02080ccec8420985bff3aa083f9c3225ca22684
Julia Grimes <julia.s.grimes@gmail.com>
2023-01-07 09:15:48 -0500

    Add image_url param to create user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     user = User.new(
       name: params[:name],
       email: params[:email],
+      image_url: params[:image_url],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
     )
