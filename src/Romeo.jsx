0ba5877a399b5ff68333461dcbb1df6cc31f5291
Julia Grimes <julia.s.grimes@gmail.com>
2023-01-08 21:23:27 -0500

    Changed description for video player

--- src/Romeo.jsx
+++ src/Romeo.jsx
@@ -2,13 +2,8 @@ export function Romeo() {
   return (
     <div>
       <iframe controls="0" src="/src/assets/Romeo.mp4" title="YouTube video player"></iframe>
-
-      <p className="pstyle1">
-        {" "}
-        One summer day we went to the aquarium, and then walked through an outside play of Romeo and Juliet. It was
-        okay, we tried to take videos through a pair of binoculars and got soup at a store in the mall, I think it had a
-        french name.{" "}
-      </p>
+      <h3>It Was Romance: Video Diary</h3>
+      <p className="pstyle1">Video diary entry from June 2018. Powered by Garageband and iMove.</p>
     </div>
   );
 }
