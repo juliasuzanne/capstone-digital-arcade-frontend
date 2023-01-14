b2d70b4744d69719b3b10199131dabec2a5aa60d
Julia Grimes <julia.s.grimes@gmail.com>
2023-01-14 09:50:08 -0500

    Revert "Troubleshooting frontend from local server, changed some file path names"
    This reverts commit 8b708ec8de224031914932d18dc389f420b016c6.

--- src/ButtonRoom.jsx
+++ src/ButtonRoom.jsx
@@ -3,8 +3,7 @@ import { Light } from "./Images/Light";
 import axios from "axios";
 import { Modal3 } from "./Modal3";
 import { Sound } from "./Sound";
-import counterjulia from "/src/assets/images/counterJulia.gif";
-import homebackground from "/src/assets/images/home_background.png";
+
 // import face from "./src/assets/images/facepainting.png";
 // import pupils from "./src/assets//images/pupilspainting.png";
 // import { autoType } from "d3";
@@ -20,9 +19,10 @@ export function ButtonRoom() {
   const [grate, setGrate] = useState("");
   const [fixed, setFixed] = useState(false);
   const [showNote, setShowNote] = useState(false);
-  const [isBoxFixed, setIsBoxFixed] = useState("");
-  const [image, setImage] = useState("./src/assets/images/closecurtainorange.png");
-  const [stairs, setStairs] = useState("./src/assets/images/stairs.png");
+  const [isBoxFixed, setIsBoxFixed] = useState(false);
+  let [array, setArray] = useState([]);
+  const [image, setImage] = useState("./assets/images/closecurtainorange.png");
+  const [stairs, setStairs] = useState("./assets/images/stairs.png");
   let [currentUser, setCurrentUser] = useState([]);
   const [items, setItems] = useState([]);
 
@@ -35,10 +35,10 @@ export function ButtonRoom() {
 
   const checkBoxFixed = () => {
     if (currentUser.fixed === true) {
-      setIsBoxFixed("./src/assets/images/closedbox.png");
+      setIsBoxFixed("./assets/images/closedbox.png");
       setFixed(true);
     } else {
-      setIsBoxFixed("./src/assets/images/openwire.png");
+      setIsBoxFixed("./assets/images/openwire.png");
       setFixed(false);
     }
   };
@@ -125,83 +125,77 @@ export function ButtonRoom() {
   useEffect(checkBoxFixed);
 
   return (
-    <div id="buttonRoom">
-      <div className="buttonRoom">
-        {localStorage.jwt === undefined ? (
-          <img className="top-box" src="./src/assets/images/openwire.png" />
-        ) : (
-          <img className="top-box" src={isBoxFixed} />
-        )}
-        {/* 
-      <img className="top-box" width="300px" src="./src/assets/images/openwire.png" /> */}
-        <div>
-          <button onClick={toggleTalking} hidden={isTalking} id="talkingtome">
-            <p>{talkText}</p>
-          </button>
-        </div>
-        <Sound />
-        <div>
-          <img src="./src/assets/images/room_unpacking.png" className="artifactRoom" />
-        </div>
-        <button onClick={exitRoom}>
-          <img id="exit" src="./src/assets/images/exit.png" />
-        </button>
-        <img src="./src/assets/images/eyespainting.png" className="eyesPaintingActual" />
-        <button className="eyesPainting" onClick={movingEyes}></button>
-        <button onClick={computer}>
-          <img id="computer" src="./src/assets/images/computer.png" />
-        </button>
-        <button onClick={handleShowLight}>
-          <img id="lightswitch" src="./src/assets/images/lightswitch.png" />
-        </button>
-        <button id="artifactbox" onClick={seeArtifacts}></button>
-        <button onClick={exitRoom}>
-          <img id="exit" src="./src/assets/images/exit.png" />
-        </button>
-        <button
-          id="opencurtain"
-          onMouseEnter={() => setImage("./src/assets/images/opencurtainorange.png")}
-          onMouseLeave={() => setImage("./src/assets/images/closecurtainorange.png")}
-          onClick={catalog}
-        ></button>
-        <button
-          id="stairs"
-          onMouseEnter={() => setStairs("./src/assets/images/stairslight.png")}
-          onMouseLeave={() => setStairs("./src/assets/images/stairs.png")}
-          onClick={exitRoom}
-        ></button>
-        <button
-          id="grate"
-          onMouseEnter={() => setGrate("./src/assets/images/eyeballgrate.png")}
-          onMouseLeave={() => setGrate("")}
-          onClick={exitRoom}
-        ></button>
+    <div className="buttonRoom">
+      {localStorage.jwt === undefined ? (
+        <img className="top-box" src="./assets/images/openwire.png" />
+      ) : (
+        <img className="top-box" src={isBoxFixed} />
+      )}
+      {/* 
+      <img className="top-box" width="300px" src="./assets/images/openwire.png" /> */}
+      <button onClick={toggleTalking} hidden={isTalking} id="talkingtome">
+        <p>{talkText}</p>
+      </button>
+      <Sound />
+      <img src="./assets/images/room_unpacking.png" className="artifactRoom" />
+      <button onClick={exitRoom}>
+        <img id="exit" src="./assets/images/exit.png" />
+      </button>
+      <img src="./assets/images/eyespainting.png" className="eyesPaintingActual" />
+      <button className="eyesPainting" onClick={movingEyes}></button>
+      <button onClick={computer}>
+        <img id="computer" src="./assets/images/computer.png" />
+      </button>
+      <button onClick={handleShowLight}>
+        <img id="lightswitch" src="./assets/images/lightswitch.png" />
+      </button>
+      <button id="artifactbox" onClick={seeArtifacts}></button>
+      <button onClick={exitRoom}>
+        <img id="exit" src="./assets/images/exit.png" />
+      </button>
+      <button
+        id="opencurtain"
+        onMouseEnter={() => setImage("./assets/images/opencurtainorange.png")}
+        onMouseLeave={() => setImage("./assets/images/closecurtainorange.png")}
+        onClick={catalog}
+      ></button>
+      <button
+        id="stairs"
+        onMouseEnter={() => setStairs("./assets/images/stairslight.png")}
+        onMouseLeave={() => setStairs("./assets/images/stairs.png")}
+        onClick={exitRoom}
+      ></button>
+      <button
+        id="grate"
+        onMouseEnter={() => setGrate("./assets/images/eyeballgrate.png")}
+        onMouseLeave={() => setGrate("")}
+        onClick={exitRoom}
+      ></button>
 
-        {/* <Curtain show={openCurtain} /> */}
-        <img className="counterJulia" src={counterjulia} />
-        <img className="counter" src="./src/assets/images/counter.png" />
-        <img className="roombackground" src={homebackground} />
-        <img className="opencurtain" src={image} />
-        <img className="stairsImage" src={stairs} />
-        <img className="grateImage" src={grate} />
-        <Light show={showLight} />
-        <button className="talk" onClick={handleShowConversation}>
-          {" "}
-          talk{" "}
-        </button>
-        <ConversationModal show={isConversationVisible} onClose={handleCloseConversation}>
-          <Home onClose={handleCloseConversation} />
-        </ConversationModal>
-        <button className="note" onClick={handleShowNote}></button>
-        <Modal3 show={showNote} onClose={handleHideNote}>
-          <ThreeD />
-        </Modal3>
-        <div id="artifactsinbox">
-          {" "}
-          {items.slice(0, 10).map((item) => (
-            <img src={item.image_url} width="30px" />
-          ))}{" "}
-        </div>
+      {/* <Curtain show={openCurtain} /> */}
+      <img className="counterJulia" src="./assets/images/counterJulia.gif" />
+      <img className="counter" src="./assets/images/counter.png" />
+      <img className="roombackground" src="./assets/images/home_background.png" />
+      <img className="opencurtain" src={image} />
+      <img className="stairsImage" src={stairs} />
+      <img className="grateImage" src={grate} />
+      <Light show={showLight} />
+      <button className="talk" onClick={handleShowConversation}>
+        {" "}
+        talk{" "}
+      </button>
+      <ConversationModal show={isConversationVisible} onClose={handleCloseConversation}>
+        <Home onClose={handleCloseConversation} />
+      </ConversationModal>
+      <button className="note" onClick={handleShowNote}></button>
+      <Modal3 show={showNote} onClose={handleHideNote}>
+        <ThreeD />
+      </Modal3>
+      <div id="artifactsinbox">
+        {" "}
+        {items.slice(0, 10).map((item) => (
+          <img src={item.image_url} width="30px" />
+        ))}{" "}
       </div>
     </div>
   );
