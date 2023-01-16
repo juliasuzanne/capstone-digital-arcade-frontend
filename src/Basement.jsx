3223406c9937817755105df7e92f34c3d9ca5f79
Julia Grimes <julia.s.grimes@gmail.com>
2023-01-16 08:22:11 -0500

    Update compressed images and adjusted css files

--- src/Basement.jsx
+++ src/Basement.jsx
@@ -1,7 +1,7 @@
 export function Basement() {
   return (
     <div>
-      <img className="roombackground" src="/images/basement.png" />
+      <img className="basementbackground" src="/images/basement.png" />
       <img src="/images/hode.gif" className="thousands" />
       <img src="/images/hode.png" className="thousands-shadow" />
     </div>
